50479be88b8c2ed4aa254b922ee9c26ac0d11c6e
Kevin <kevinrivadeneira724@gmail.com>
2026-08-19 16:03:35 -0600

    Initialize frontend structure with HTML, Vite, Tailwind CSS, and React setup

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js, ts, jsx,tsx}",
+
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6366f1',
+                success: '#22c55e',
+                danger: '#ef4444',
+                warning: '#f59e0b',
+            }
+        },
+    },
+    plugins: [],
+}
