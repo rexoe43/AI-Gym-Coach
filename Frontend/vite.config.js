fe0f9d4b3a15b08545c73c295adcbb5b3b7a32ac
Kevin <kevinrivadeneira724@gmail.com>
2026-08-20 09:55:07 -0600

    Fix dependencies section in package.json and restore vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -16,5 +16,8 @@ export default defineConfig({
                 changeOrigin: true,
             }
         }
+    },
+    optimizeDeps: {
+        include: ['react', 'react-dom', 'react/jsx-runtime']
     }
 })
